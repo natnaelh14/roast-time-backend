5c16e7ac1b79c631d897bb77dd85172153ff3830
natnaelh14 <natnaelh14@github.com>
2022-09-24 14:49:16 -0400

    initial jwt setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,45 @@
+require('dotenv').config();
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const app = express();
 
+//It lets the app use json form the body of the request.
+app.use(express.json());
+
+const posts = [
+  {
+    username: 'Bob',
+    title: 'Post One',
+  },
+  {
+    username: 'Aaron',
+    title: 'Post Two',
+  },
+];
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  //It will Bearer {{token}}
+  const token = authHeader && authHeader.split(' ')[1]; //If there is not token, it will be undefined.
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+app.post('/login', (req, res) => {
+  //Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.SECRET_TOKEN);
+  res.json({ accessToken });
+});
+
+
 app.listen(3002);
-
-
