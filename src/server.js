f46823a8c586aff52dacb8a140d650c5c9487d62
natnaelh14 <natnaelh14@github.com>
2022-09-25 11:34:23 -0400

    implemented cors restrictions

--- src/server.js
+++ src/server.js
@@ -2,9 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
+const PORT = process.env.PORT || 3500;
+const credentials = require('./middleware/credentials');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const cookieParser = require('cookie-parser');
+
+// custom middleware logger
+// app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
 
 //It lets the app use json form the body of the request.
 app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 const posts = [
   {
@@ -28,8 +52,15 @@ const authenticateToken = (req, res, next) => {
     next();
   });
 };
+
+app.use(verifyJWT);
+
+
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter((post) => post.username === req.user.name));
 });
 
-app.listen(3002);
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
