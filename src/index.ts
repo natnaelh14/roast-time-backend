edfd374bfbeb8e8cb92e358b823955cdbbc6cc63
natnaelh14 <natnaelh14@github.com>
2024-03-30 19:38:00 -0400

    fix render build error

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
 import * as dotenv from "dotenv";
 import app from "./app";
-// import fs from 'fs';
-// import https from 'https';
-
 const PORT = process.env.PORT || 3009;
 
 dotenv.config();
-
-// const server = https
-//   .createServer(
-//     {
-//       key: fs.readFileSync('key.pem'),
-//       cert: fs.readFileSync('cert.pem'),
-//     },
-//     app,
-//   )
-//   .listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-// server.setTimeout(120000);
 
 const server = app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 server.setTimeout(120000);
